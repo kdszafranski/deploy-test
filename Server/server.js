fe3c994337350ff07f0f6ae93fc6ab563a967c85
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-05-31 23:59:50 -0500

    code comments edited

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,4 @@
+// importing modules
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,6 +8,7 @@ const taskRouter = require('./routes/task.router.js')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('Server/Public'));
 
+// diverting traffic to taskRouter as defined above
 app.use('/tasks', taskRouter)
 
 app.listen(PORT, () => {
