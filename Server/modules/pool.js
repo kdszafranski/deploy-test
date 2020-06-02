47346470c57164285c1cc9f31c4d56772f4f7777
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-06-01 19:56:42 -0500

    heroku deployment updates

--- Server/modules/pool.js
+++ Server/modules/pool.js
@@ -1,17 +1,36 @@
 // importing pg module
 const pg = require('pg');
-
 const Pool = pg.Pool;
+const url = require('url');
 
-// creating a new instance of pool with database attributes
-const pool = new Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
+let config = {};
 
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: { rejectUnauthorized: false },
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+} else {
+    config = {
+        host: 'localhost', // Server hosting the postgres database
+        port: 5432, // env var: PGPORT
+        database: 'weekend-to-do-app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+}
+
+const pool = new Pool(config);
 // establishing a connection to the database
 pool.on('connect', () => {
     console.log('Database connected');
